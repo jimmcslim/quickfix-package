e7af0937c313dcae94d8072dcb766cec47679abb
James Webster <jim@agilewallaby.net>
2022-03-21 14:34:45 +1000

    Don't attempt to upload.

--- package-python.sh
+++ package-python.sh
@@ -25,4 +25,4 @@ rm -f quickfix-python/C++/stdafx.*
 
 pushd quickfix-python
 python setup.py sdist
-PYTHONWARNINGS="ignore" twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# PYTHONWARNINGS="ignore" twine upload --repository-url https://test.pypi.org/legacy/ dist/*
